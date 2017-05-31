620f446dcd6512f33eb6b3967ea8777bf0a88167
crazyyy <crazyyy@gmail.com>
2017-05-31 17:27:53 +0300

    stop task andwork with siterip

--- wordpress/wp-content/themes/wp-managym/header.php
+++ wordpress/wp-content/themes/wp-managym/header.php
@@ -23,6 +23,33 @@
 <body <?php body_class(); ?>>
 <!-- wrapper -->
 <div class="wrapper">
+
+  <div class="header-top">
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-12 col-md-12">
+          <ul class="header-top--nav">
+            <li class="active"><a href="">НАШИ КУРСЫ</a></li>
+            <li><a href="">PROJECTOR</a></li>
+            <li><a href="">MANAGYM 2</a></li>
+            <li><a href="">bePUBLIC</a></li>
+            <li><a href="">5 ОШИБОК РУКОВОДИТЕЛЯ</a></li>
+          </ul>
+        </div><!-- /.col-lg-12 col-md-12 -->
+      </div><!-- /.row -->
+    </div><!-- /.container -->
+  </div><!-- /.header-top -->
+
+  <div class="header-redzone">
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-12 col-md-12">
+          <h4>Новый курс управленческих навыков <span class="header-redzone--title">MANAGYM</span><span class="header-redzone--date">22 сентября<br>новый набор</span></h4>
+        </div><!-- /.col-lg-12 col-md-12 -->
+      </div><!-- /.row -->
+    </div><!-- /.container -->
+  </div><!-- /.header-redzone -->
+
   <header role="banner">
     <div class="inner">
 
