6fc31cfb1b5549ff7b33a5a7053312c54c1205f6
crazyyy <crazyyy@gmail.com>
2017-06-04 23:51:08 +0300

    sic

--- wordpress/wp-content/themes/wp-managym/footer.php
+++ wordpress/wp-content/themes/wp-managym/footer.php
@@ -1,19 +1,93 @@
-    </div><!-- /.inner -->
-  </section><!-- /section -->
+  <div class="clients">
+    <div class="cl_head">Наши клиенты</div>
+    <div class="nh1">
+      <img src="img/1.jpg" alt="">
+      <img src="img/2.jpg" alt="">
+      <img src="img/3.jpg" alt="">
+      <img src="img/4.jpg" alt="">
+      <img src="img/5.jpg" alt="">
+      <img src="img/6.jpg" alt="">
+      <img src="img/7.jpg" alt="">
+      <img src="img/8.jpg" alt="">
+      <img src="img/9.jpg" alt="">
+      <img src="img/10.jpg" alt="">
+      <img src="img/11.jpg" alt="">
+      <img src="img/12.jpg" alt="">
+      <img src="img/13.jpg" alt="">
+      <img src="img/14.jpg" alt="">
+      <img src="img/15.jpg" alt="">
+      <img src="img/16.jpg" alt="">
+      <img src="img/17.jpg" alt="">
+      <img src="img/18.jpg" alt="">
+      <img src="img/19.jpg" alt="">
+      <img src="img/20.jpg" alt="">
+      <img src="img/new1.jpg" alt="">
+      <img src="img/new2.jpg" alt="">
+      <img src="img/new3.jpg" alt="">
+      <img src="img/new4.jpg" alt="">
+      <img src="img/new5.jpg" alt="">
+      <img src="img/new6.jpg" alt="">
+    </div>
+  </div>
 
-</div><!-- /wrapper -->
+  <div id="map">
+    <div class="elka"></div>
+    <div class="wrap">
+      <div id="contacts">
+        <div class="addr">
+          г. Минск
+          <br> ул. Толбухина, 2, ,
+          <br> Бизнес-центр Time эт. 5
+        </div>
+        <div class="inner">
+          <div class="label">
+            Телефон:
+          </div>
+          <div class="phones">
+            <div class="tel">+375 29 68-34-600</div>
+          </div>
+          <div class="label">
+            E-mail:
+          </div>
+          <a href="mailto:managym.by@gmail.com">managym.by@gmail.com</a>
+          <br>
+          <div class="clear"></div>
+          <div class="label" style="clear:both;padding-top: 2px">Соц.сети:</div>
+          <!--noindex-->
+          <a rel="nofollow" href="https://www.facebook.com/managym.by?ref=hl#!/managym.by" style="text-decoration:underline;display:block;float:left;padding: 4px 3px 3px 0;" target="_blank">Facebook</a>
+          <a rel="nofollow" href="https://vk.com/public67648684#/public67648684" style="text-decoration:underline;display:block;float:left;padding: 4px 3px 3px 7px;" target="_blank">ВКонтакте</a>
+          <!--/noindex-->
+          <div class="clear"></div>
+        </div>
+        <a href="#" class="hide open">
+          <span>контактная информация</span>
+          <i></i>
+        </a>
+        <div class="clear"></div>
+      </div>
+    </div>
+    <div id="myMap"></div>
+    <div class="ontop" onclick="ScrollTO('body')"></div>
+  </div>
 
-<footer role="contentinfo">
-  <div class="inner">
+  <div class="footer" style="font-size: 14px;font-family: Arial;padding: 20px 0;overflow: hidden;margin-bottom: -17px;">
+    <div class="wrap">
+      <div style="float:left">&copy; Managym, 2015</div>
+      <div style="float:right"><a href="http://piplos.by/" style="border:none;text-decoration:underline;">Разработка сайта Piplos Media</a></div>
+    </div>
+  </div>
 
-    <p class="copyright">
-      &copy; <?php echo date("Y"); ?> Собственность <?php bloginfo('name'); ?>.
-    </p><!-- /copyright -->
+  <?php wp_footer(); ?>
 
-  </div><!-- /.inner -->
-</footer><!-- /footer -->
-
-    <?php wp_footer(); ?>
-
+  <script src="js/jquery.maskedinput.min.js"></script>
+  <script src="js/jquery.form.js"></script>
+  <script src="js/inline_5.js"></script>
+  <script src="js/inline_6.js"></script>
+  <script src="js/inline_7.js"></script>
+  <script src="js/inline_8.js"></script>
+  <script src="js/inline_9.js"></script>
+  <script src="js/inline_10.js"></script>
+  <script src="js/inline_11.js"></script>
 </body>
+
 </html>
--- wordpress/wp-content/themes/wp-managym/header.php
+++ wordpress/wp-content/themes/wp-managym/header.php
@@ -5,6 +5,7 @@
   <meta http-equiv="x-ua-compatible" content="ie=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1">
 
+
   <title><?php wp_title( '' ); ?><?php if ( wp_title( '', false ) ) { echo ' :'; } ?> <?php bloginfo( 'name' ); ?></title>
 
   <link href="http://www.google-analytics.com/" rel="dns-prefetch"><!-- dns prefetch -->
@@ -21,54 +22,67 @@
   <?php wp_head(); ?>
 </head>
 <body <?php body_class(); ?>>
-<!-- wrapper -->
-<div class="wrapper">
 
-  <div class="header-top">
-    <div class="container">
-      <div class="row">
-        <div class="col-lg-12 col-md-12">
-          <ul class="header-top--nav">
-            <li class="active"><a href="">НАШИ КУРСЫ</a></li>
-            <li><a href="">PROJECTOR</a></li>
-            <li><a href="">MANAGYM 2</a></li>
-            <li><a href="">bePUBLIC</a></li>
-            <li><a href="">5 ОШИБОК РУКОВОДИТЕЛЯ</a></li>
-          </ul>
-        </div><!-- /.col-lg-12 col-md-12 -->
-      </div><!-- /.row -->
-    </div><!-- /.container -->
-  </div><!-- /.header-top -->
+  <div class="upper-bar-courses">
+    <div class="upper-bar-wrapper">
+      <div>НАШИ КУРСЫ</div>
+      <table>
+        <tbody>
+          <tr>
+            <td><a href="http://www.projector.by/">PROJECTOR</a></td>
+            <td><a href="managym2/index.html">MANAGYM 2</a></td>
+            <td><a href="bepublic/index.html">bePUBLIC</a></td>
+            <td><a href="mistake/index.html">5 ОШИБОК РУКОВОДИТЕЛЯ</a></td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  </div>
 
-  <div class="header-redzone">
-    <div class="container">
-      <div class="row">
-        <div class="col-lg-12 col-md-12">
-          <h4>Новый курс управленческих навыков <span class="header-redzone--title">MANAGYM</span><span class="header-redzone--date">22 сентября<br>новый набор</span></h4>
-        </div><!-- /.col-lg-12 col-md-12 -->
-      </div><!-- /.row -->
-    </div><!-- /.container -->
-  </div><!-- /.header-redzone -->
+  <div id="up_ban">
+    <div class="cen_up_ban">
+      <h1><span class="lf">Новый курс управленческих навыков</span></h1>
+      <span class="cn" style="left: 562px">MANAGYM</span>
+      <div class="rg">22<span>сентября<br>новый набор</span>
+      </div>
+    </div>
+  </div>
 
-  <header role="banner">
-    <div class="inner">
+  <div class="overlay"></div>
 
-      <div class="logo">
-        <?php if ( !is_front_page() && !is_home() ){ ?>
-          <a href="<?php echo home_url(); ?>">
-        <?php } ?>
-            <img src="<?php echo get_template_directory_uri(); ?>/img/logo.png" alt="<?php wp_title( '' ); ?>" title="<?php wp_title( '' ); ?>" class="logo-img">
-        <?php if ( !is_front_page() && !is_home() ){ ?>
-          </a>
-        <?php } ?>
-      </div><!-- /logo -->
+  <header class="top_managym_bg">
+    <div class="wrap">
+      <div class="nh3" style="top: 90px;">
+        <div style="margin-bottom: -20px;"><span style="border-bottom: 2px solid red;">managym</span> —</div>
+        <br> КУРС управленческих
+        <br>навыков</div>
+      <div class="nh4">Прокачиваем молодых управленцев
+        <br>для корпоративной борьбы</div>
+      <p class="quote" style="margin:25px 0px 0px 0px;">
+        «Снесём крышу до фундамента, на нём построим
+        <br>все как нужно».
+        <span class="author">— Юрий Анушкин, тренер</span>
+      </p>
+      <div class="date" style="width:200px;">
+        <span class="head-date" style="text-align: right; width:200px;">22<b style="text-align: left">сентября<br><span>новый<br>набор</span></b>
+        </span>
+      </div>
+      <a href="#schedule" class="signup" onclick="ScrollTO('.bott_form_succes'); return false;" id="top_reg_btn">Записаться</a>
+    </div>
+    <div class="img" id="managym_top_banner"></div>
+    <div class="clear"></div>
+  </header>
 
-      <nav class="nav" role="navigation">
-        <?php wpeHeadNav(); ?>
-      </nav><!-- /nav -->
-
-    </div><!-- /.inner -->
-  </header><!-- /header -->
-
-  <section role="main">
-    <div class="inner">
+  <nav>
+    <b></b>
+    <ul>
+      <li><a href="#" onclick="ScrollTO('#center'); return false;">Тренинг-центр</a></li>
+      <li><a href="#" onclick="ScrollTO('#coach');return false;">Тренер</a></li>
+      <li><a href="#" onclick="ScrollTO('.kurs-programm');return false;">Программа</a></li>
+      <li><a href="#" onclick="ScrollTO('#contacts');return false;">Контакты</a></li>
+      <li><a href="mistake/index.html" target="_blank">Видеокурс</a></li>
+      <li><a href="managym2/index.html" target="_blank">Managym2</a></li>
+      <li class="clear"></li>
+    </ul>
+    <i></i>
+  </nav>
