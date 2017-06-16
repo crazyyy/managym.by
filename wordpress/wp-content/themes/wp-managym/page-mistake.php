d2f6e06e59a37862eeb762a435b38332240a0762
Toy Mach1ne <forvinwolves@gmail.com>
2017-06-16 20:00:49 +0300

    page mistake, page managym2 (in progress)

---
+++ wordpress/wp-content/themes/wp-managym/page-mistake.php
@@ -0,0 +1,49 @@
+<?php /* Template Name: mistake */ get_header(); ?>
+<div class="header">
+  <div class="container">
+    <div class="row">
+      <div class="col-md-12">
+        <div class="header_inner_wrap">
+          <p class="first_row">Бесплатный Видеокурс</p>
+          <h1>5 ТИПИЧНЫХ ОШИБОК РУКОВОДИТЕЛЯ</h1>
+          <p class="underslogan">КАК ИЗБЕЖАТЬ ОШИБОК, КОТОРЫЕ МОГУТ СТОИТЬ ВАМ ЦЕЛОЙ КОМПАНИИ</p>
+        </div>
+      </div>
+    </div>
+  </div>
+</div><!-- header -->
+
+<div class="white_gray">
+  <div class="container">
+    <div class="row">
+      <div class="col-md-6">
+        <div class="block_inner_wrap">
+          <h4>ИЗ КУРСА ВЫ УЗНАЕТЕ:</h4>
+          <ul>
+            <li><span>От специалиста к руководителю. Как быстро перейти эту грань.</span></li>
+            <li><span>Нужно ли вести дружеские отношения с сотрудниками.</span></li>
+            <li><span>Что делать, если вам проще сделать самому, чем объяснить другим.</span></li>
+            <li><span>Когда вы имитируете поведение руководителя. Чем вам это грозит?</span></li>
+            <li><span>Как перестать опираться на эмоции и работать только с фактами.</span></li>
+          </ul>
+        </div>
+      </div>
+      <div class="col-md-6">
+        <div class="second_inner_wrap">
+          <?php echo do_shortcode('[contact-form-7 id="189" title="БЕСПЛАТНЫЙ ВИДЕОКУРС"]'); ?>
+        </div>
+      </div>
+    </div>
+  </div>
+</div><!-- white_gray -->
+
+<div class="mistake-footer">
+  <div class="container">
+    <div class="row">
+      <div class="col-md-12">
+        <p>+375 29 68-34-600</p>
+      </div>
+    </div>
+  </div>
+</div>
+<?php get_footer(); ?>
